783babbc53ec3d38f6ded624973246375f45de6a
chad-tung <sven.tung@gmail.com>
2020-02-06 11:11:50 +0000

    Adds class abstract Class

--- src/main/java/Class/Class.java
+++ src/main/java/Class/Class.java
@@ -1,4 +1,31 @@
 package Class;
 
-public class Class {
+import Behaviours.IConsume;
+import Behaviours.IHealth;
+import Behaviours.IWield;
+import Potion.Potion;
+import Weapon.Weapon;
+
+import java.util.ArrayList;
+
+public abstract class Class implements IHealth, IWield, IConsume{
+
+    ArrayList<Weapon> weapons;
+    ArrayList<Potion> potions;
+
+    int HP;
+
+
+    public Class(ArrayList<Weapon> weapons, ArrayList<Potion> potions, int HP){
+        this.weapons = weapons;
+        this.potions = potions;
+        this.HP = HP;
+    }
+
+    public void attack(){}
+
+    public void healDamage(){}
+
+    public void takeDamage(){}
+
 }
